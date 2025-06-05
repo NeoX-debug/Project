28336370f33e13aeac43149b1908d9763f4a533e
NeoX <forlannoums@gmail.com>
2025-06-05 15:35:08 +0100

    Website update

--- JS/acceuil.js
+++ JS/acceuil.js
@@ -16,11 +16,46 @@ function toggleSubmenu(event, submenuId) {
 }
 
 
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' +counter).checked=true;
-    counter++;
-    if(counter>4){
-        counter=1
-    }
-},5000)
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
+let slideInterval = setInterval(() => changeSlide(1), 5000);
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+        dots[i].classList.toggle('active', i === index);
+    });
+    currentSlide = index;
+}
+
+function changeSlide(n) {
+    let newIndex = (currentSlide + n + slides.length) % slides.length;
+    showSlide(newIndex);
+    resetInterval();
+}
+
+function goToSlide(n) {
+    showSlide(n);
+    resetInterval();
+}
+
+function resetInterval() {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(() => changeSlide(1), 5000);
+}
+
+// Touch support for mobile
+let startX = 0;
+document.querySelector('.slides').addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+});
+document.querySelector('.slides').addEventListener('touchend', (e) => {
+    let endX = e.changedTouches[0].clientX;
+    if (endX - startX > 50) changeSlide(-1);
+    else if (startX - endX > 50) changeSlide(1);
+});
+
+// Initialize
+showSlide(currentSlide);
